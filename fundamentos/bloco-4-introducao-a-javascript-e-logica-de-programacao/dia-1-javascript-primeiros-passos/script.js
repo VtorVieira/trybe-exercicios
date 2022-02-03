a2e46715aa9ad78756aef23b4e965b631dfbe82c
Vitor <vieira.vitor42@gmail.com>
2022-02-03 10:19:06 -0300

    Exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,16 +1,9 @@
-const porcent = 91;
-if(porcent >= 90){
-    console.log("A");
-}else if(porcent >= 80){
-    console.log("B")
-}else if(porcent >= 70){
-    console.log("C")
-}else if(porcent >= 60){
-    console.log("D")
-}else if(porcent >= 50){
-    console.log("E")
-}else if(porcent < 50){
-    console.log("F")
+const num1 = 9;
+const num2 = 9;
+const num3 = 9;
+
+if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
+    console.log("É Par");
 }else{
-    console.log("A nova passada não pode ser menos que 0 e mais que 100");
+    console.log("É Impar");
 }
