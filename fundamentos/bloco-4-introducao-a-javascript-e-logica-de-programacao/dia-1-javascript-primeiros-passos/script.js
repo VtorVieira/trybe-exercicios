a8af707bacc0363223a8e54b5543b84be23bf756
Vitor <vieira.vitor42@gmail.com>
2022-02-03 18:17:57 -0300

    Exercicio 11

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,18 +1,28 @@
+let salarioBruto = 5189.82;
+let salarioLiquido;
 
-let peca = "CAVALO";
-peca = peca.toLowerCase();
-if(peca == "peao"){
-    console.log("anda uma pra frente ou duas na primeira jogada")
-} else if(peca == "torre"){
-    console.log("anda quantas casas quiser pra frente ou para o lado");
-} else if(peca == "cavalo"){
-    console.log("anda em L");
-} else if(peca == "bispo"){
-    console.log("anda na diagonal");
-} else if(peca == "rainha"){
-    console.log("anda quantas casas quiser e para todas as direções");
-} else if(peca == "rei"){
-    console.log("anda uma casa para todas as direções");
-} else{
-    console.log("Peça invalida!");
+if (salarioBruto <= 1556.94) {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.08);
+    console.log("Abaixo de " + salarioLiquido);
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.09);
+    console.log("Abaixo de " + salarioLiquido);
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.11);
+    console.log("Salario liquido " + salarioLiquido);
+    if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65) {
+        salarioLiquido = salarioLiquido - (salarioLiquido * 0.075 - 142.80);
+    } else if(salarioLiquido >= 2826.66 && salarioLiquido <= 3751.05){
+        salarioLiquido = salarioLiquido - (salarioLiquido * 0.15 - 354.80);
+    } else if(salarioLiquido >= 3751.06 && salarioLiquido <= 4664.68){
+        salarioLiquido = salarioLiquido - (salarioLiquido * 0.225 - 636.13);
+    }
+    console.log("Atualizado salario liquido " + salarioLiquido);
+} else if (salarioBruto >= 5189.82) {
+    salarioLiquido = salarioBruto - (salarioBruto - 570.88);
+    console.log("Salario liquido " + salarioLiquido);
+    if(salarioLiquido > 4664.68){
+        salarioLiquido = salarioLiquido - (salarioLiquido * 0.225 - 354.80);
+    }
+    console.log("Atualizado salario liquido " + salarioLiquido);
 }
