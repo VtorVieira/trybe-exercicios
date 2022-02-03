1c643fa6e94a4d910f43a260e50f489e8bf32bda
Vitor <vieira.vitor42@gmail.com>
2022-02-03 10:20:45 -0300

    Exercicio 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,9 @@
 const num1 = 9;
 const num2 = 9;
-const num3 = 9;
+const num3 = 6;
 
-if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
-    console.log("É Par");
-}else{
+if(num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0){
     console.log("É Impar");
+}else{
+    console.log("É Par");
 }
