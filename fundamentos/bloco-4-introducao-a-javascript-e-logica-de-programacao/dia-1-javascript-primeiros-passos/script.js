eb8280c7f8e35d5267b48c4843b344bae554d087
Vitor <vieira.vitor42@gmail.com>
2022-02-03 10:14:20 -0300

    Exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,11 +1,16 @@
-const ladoA = 60;
-const ladoB = 60;
-const ladoC = 60;
-
-let soma = ladoA + ladoB + ladoC;
-
-if(soma == 180){
-    console.log(true);
+const porcent = 91;
+if(porcent >= 90){
+    console.log("A");
+}else if(porcent >= 80){
+    console.log("B")
+}else if(porcent >= 70){
+    console.log("C")
+}else if(porcent >= 60){
+    console.log("D")
+}else if(porcent >= 50){
+    console.log("E")
+}else if(porcent < 50){
+    console.log("F")
 }else{
-    console.log(false);
+    console.log("A nova passada não pode ser menos que 0 e mais que 100");
 }
