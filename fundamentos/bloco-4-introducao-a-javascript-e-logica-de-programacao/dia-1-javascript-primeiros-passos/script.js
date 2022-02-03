173f91b7e295b1a7353c2ec15426dc2034ea7624
Vitor <vieira.vitor42@gmail.com>
2022-02-03 09:46:36 -0300

    Exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 6;
+let soma = a + b;
+let sub = a- b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log('A soma é: ' + soma);
+console.log('A subtração é: ' + sub);
+console.log('A multiplicação é: ' + mult);
+console.log('A divisão é: ' + div);
+console.log('O modulo é: ' + mod);
