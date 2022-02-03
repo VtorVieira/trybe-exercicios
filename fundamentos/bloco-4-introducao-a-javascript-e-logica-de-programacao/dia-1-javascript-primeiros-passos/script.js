47f4111f21960c7eda842f9683688ea252753202
Vitor <vieira.vitor42@gmail.com>
2022-02-03 09:53:04 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,8 +1,11 @@
 const a = 7;
-const b = 6;
+const b = 9;
+const c = 8;
 
-if(a > b){
-    console.log("A const A é maior que a B");
+if(a > b && a > c){
+    console.log("A const A é a maior: " + a);
+}else if(b > c){
+    console.log("A const B é a maior: " + b);
 }else{
-    console.log("A const B é maior que a A");
+    console.log("A const C é a maior: " + c);
 }
