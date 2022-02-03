58602d98906d1fbc6dc3a3f52e7d6cd51c00352c
Vitor <vieira.vitor42@gmail.com>
2022-02-03 09:48:35 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,13 +1,8 @@
-const a = 5;
+const a = 7;
 const b = 6;
-let soma = a + b;
-let sub = a- b;
-let mult = a * b;
-let div = a / b;
-let mod = a % b;
 
-console.log('A soma é: ' + soma);
-console.log('A subtração é: ' + sub);
-console.log('A multiplicação é: ' + mult);
-console.log('A divisão é: ' + div);
-console.log('O modulo é: ' + mod);
+if(a > b){
+    console.log("A const A é maior que a B");
+}else{
+    console.log("A const B é maior que a A");
+}
