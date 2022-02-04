8195290b05108608cdd25c6e4b072845fc6c9383
Vitor <vieira.vitor42@gmail.com>
2022-02-04 19:18:33 -0300

    Exercicio 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scritp.js
@@ -0,0 +1,69 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1
+// for(index = 0; index < numbers.length; index++){
+//     console.log(numbers[index]);
+// }
+
+// Exercicio 2
+// let resultado = 0;
+// for (index = 0; index < numbers.length; index++) {
+//   resultado = resultado + numbers[index]
+// }
+// console.log(resultado);
+
+//Exercicio 3
+// let resultado = 0;
+// for (index = 0; index < numbers.length; index++) {
+//   resultado = (resultado + numbers[index]) / numbers.length;
+// }
+// console.log(resultado);
+
+//Exericico 4
+// let resultado = 0;
+// for (index = 0; index < numbers.length; index++) {
+//   resultado = (resultado + numbers[index]) / numbers.length;
+// }
+// if(resultado > 20){
+//   console.log("valor maior que 20");
+// }else{
+//   console.log("valor meno que 20");
+// }
+
+//Exericico 5
+// let maior = 0;
+// for(index = 0; index < numbers.length; index += 1 ){
+//   if(numbers[index] > maior){
+//     maior = numbers[index];
+//   }
+// }
+// console.log(maior);
+
+//Exericico 6
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     console.log(numbers[index]);
+//   } else {
+//     console.log("Não é um numero impar!");
+//   }
+// }
+
+//Exericico 7
+// let menor = numbers[0];
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] < menor) {
+//     menor = numbers[index];
+//   }
+// }
+// console.log(menor);
+
+//Exercicio 8
+// for(index = 1; index <= 25; index +=1){
+//   console.log(index);
+// }
+
+//Exercicio 9
+// for(index = 1; index <= 25; index +=1){
+//   var divisao = ("a divisao de " + index + "/2 = " + index  / 2);
+//   console.log(divisao);
+// }
