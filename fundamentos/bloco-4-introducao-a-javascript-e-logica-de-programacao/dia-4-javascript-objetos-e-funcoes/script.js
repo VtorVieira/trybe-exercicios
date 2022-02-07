c39c4eefce98898b53fea8c606c61e0602f7f3ea
Vitor <vieira.vitor42@gmail.com>
2022-02-07 12:24:11 -0300

    Parte I - exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -4,8 +4,15 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 
 };
+//Exercicio 1
 console.log("Bem-vinda, " + info.personagem);
 
+//Exercicio 2
 info['recorrente'] = 'Sim'; //adicionado a propriedade recorrente com o valor 'sim'
 
-console.log(info);
+/*console.log(info);*/
+
+//Exercicio 3
+for(let key in info){
+    console.log(info[key]);
+}
