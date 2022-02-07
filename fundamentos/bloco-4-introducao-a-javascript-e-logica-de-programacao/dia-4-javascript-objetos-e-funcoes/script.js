75016dd9954d21c706b7963af025f7004de4d178
Vitor <vieira.vitor42@gmail.com>
2022-02-07 12:22:19 -0300

    Parte I - exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -2,5 +2,10 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+
 };
 console.log("Bem-vinda, " + info.personagem);
+
+info['recorrente'] = 'Sim'; //adicionado a propriedade recorrente com o valor 'sim'
+
+console.log(info);
