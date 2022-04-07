a92621dc6cf363d7218d8a74f0b3bc9c807a2314
Vitor <vieira.vitor42@gmail.com>
2022-04-07 13:49:31 -0300

    fix-11-1

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import React from 'react';
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      numeroEventClick: 0,
+      numeroEventClickEmMim: 0,
+      numeroEventNaoClick: 0
+    }
+    this.eventClick = this.eventClick.bind(this);
+    this.eventClickEmMim = this.eventClickEmMim.bind(this);
+    this.eventNaoClick = this.eventNaoClick.bind(this);
+  }
+
+  eventClick() {
+    const btn = document.getElementsByTagName('button')[0];
+    this.setState((beforeState) => ({
+      numeroEventClick: beforeState.numeroEventClick + 1
+    }))
+    if (this.state.numeroEventClick % 2 !== 0) {
+      btn.className = 'colorGreen';
+    } else {
+      btn.className = '';
+    }
+  }
+  eventClickEmMim() {
+    this.setState((beforeState) => ({
+      numeroEventClickEmMim: beforeState.numeroEventClickEmMim + 1
+    }))
+  }
+  eventNaoClick() {
+    this.setState((beforeState) => ({
+      numeroEventNaoClick: beforeState.numeroEventNaoClick + 1
+    }))
+  }
+
+  eventColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
+  render() {
+
+    return (
+      <div>
+        <button onClick={this.eventClick}>{this.state.numeroEventClick}</button>
+        <button onClick={this.eventClickEmMim}>{this.state.numeroEventClickEmMim}</button>
+        <button onClick={this.eventNaoClick}>{this.state.numeroEventNaoClick}</button>
+      </div>
+    )
+  }
+}
+
+export default App;
