ddbf4b40e615b8394fccbf216bd645826f97e980
Vitor <vieira.vitor42@gmail.com>
2022-04-13 15:03:55 -0300

    Testando commit geral

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-// import Pessoais from './Pessoais';
+import Pessoais from './Pessoais';
 
 class App extends Component {
   constructor() {
@@ -25,7 +25,6 @@ class App extends Component {
       cargo: '',
       descricao: '',
       countCargo: '',
-
     }
   }
 
@@ -89,46 +88,17 @@ class App extends Component {
   }
 
   render() {
-    const { nome, email, cpf, endereco, cidade, estado, resumo, descricao, cargo } = this.state;
-    console.log(descricao.length)
-    let error = undefined
-    if (descricao.length > 120) error = " Texto muito grande"
-
     return (
       <form>
-        <fieldset>
-          <legend>Dados Pessoais</legend>
-          <input onChange={this.handleChange} value={nome} name='nome' type='text' maxLength='40' placeholder='nome' required></input>
-          <input onChange={this.handleChange} value={email} name='email' type='email' maxLength='50' placeholder='Email' required></input>
-          <input onChange={this.handleChange} value={cpf} name='cpf' type='text' maxLength='11' placeholder='CPF' required></input>
-          <input onChange={this.removeCaracter} value={endereco} name='endereco' type='text' maxLength='20' placeholder='Endereco' required></input>
-          <input onBlur={this.removeFocus} onChange={this.handleChange} value={cidade} name='cidade' type='text' maxLength='28' placeholder='Cidade' required></input>
-          <select onChange={this.handleChange} value={estado} name='estado' required>
-            <option value='sp'>SP</option>
-            <option value='ba'>BA</option>
-            <option value='rj'>RJ</option>
-          </select>
-          <div>
-            <input onChange={this.onValueChange} id='casa' name='tipo' type='radio' required></input>
-            <label for='casa' >Casa</label>
-            <input onChange={this.onValueChange} id='apartamento' name='tipo' type='radio' required></input>
-            <label for='apartamento'>Apartamento</label>
-          </div>
-        </fieldset>
-        <fieldset>
-          <legend>Dados Último emprego</legend>
-          <textarea onChange={this.handleChange} value={resumo} name='resumo' rows='2' cols='50' maxLength='1000' placeholder='Resumo' required></textarea>
-          <textarea onMouseOver={this.alertMsg} onChange={this.handleChange} value={cargo} name='cargo' rows='2' cols='50' maxLength='40' placeholder='Cargo' required></textarea>
-          <textarea onChange={this.handleChange} value={descricao} name='descricao' rows='2' cols='50' maxLength='500' placeholder='Descricao' required></textarea>
-          <span>{error ? error : ''}</span>
-        </fieldset>
-        <button onSubmit={this.sendButton}>Submit</button>
-        <button onSubmit={this.resetButton}>Reset</button>
-        {/* <Pessoais value={this.state}
-          onKeyUpValue={this.onKeyUpValue}
-          removeCaracter={this.removeCaracter} onValueChange={this.onValueChange}
-          removeFocus={this.removeFocus} alertMsg={this.alertMsg} resetButton={this.resetButton}
-        /> */}
+
+        <Pessoais value={this.state}
+          handleChange={this.handleChange}
+          removeCaracter={this.removeCaracter}
+          onValueChange={this.onValueChange}
+          removeFocus={this.removeFocus}
+          alertMsg={this.alertMsg}
+          resetButton={this.resetButton}
+        />
       </form>
     )
   }
--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Pessoais.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Pessoais.jsx
@@ -2,10 +2,40 @@ import { Component } from "react";
 
 class Pessoais extends Component {
   render() {
-    const { nome, email, cpf, endereco, cidade, estado, resumo, descricao, cargo,
-      onKeyUpValue, handleChange, removeCaracter, removeFocus, onValueChange, alertMsg, resetButton } = this.props
-    return (<div>
+    const { value, handleChange, removeCaracter, removeFocus, onValueChange, alertMsg, resetButton } = this.props
 
+    let error = undefined
+    if (value.descricao.length > 120) error = " Texto muito grande"
+
+    return (<div>
+      <fieldset>
+        <legend>Dados Pessoais</legend>
+        <input onChange={handleChange} value={value.nome} name='nome' type='text' maxLength='40' placeholder='nome' required></input>
+        <input onChange={handleChange} value={value.email} name='email' type='email' maxLength='50' placeholder='Email' required></input>
+        <input onChange={handleChange} value={value.cpf} name='cpf' type='text' maxLength='11' placeholder='CPF' required></input>
+        <input onChange={removeCaracter} value={value.endereco} name='endereco' type='text' maxLength='20' placeholder='Endereco' required></input>
+        <input onBlur={removeFocus} onChange={handleChange} value={value.cidade} name='cidade' type='text' maxLength='28' placeholder='Cidade' required></input>
+        <select onChange={handleChange} value={value.estado} name='estado' required>
+          <option value='sp'>SP</option>
+          <option value='ba'>BA</option>
+          <option value='rj'>RJ</option>
+        </select>
+        <div>
+          <input onChange={onValueChange} id='casa' name='tipo' type='radio' required></input>
+          <label for='casa' >Casa</label>
+          <input onChange={onValueChange} id='apartamento' name='tipo' type='radio' required></input>
+          <label for='apartamento'>Apartamento</label>
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Dados Último emprego</legend>
+        <textarea onChange={handleChange} value={value.resumo} name='resumo' rows='2' cols='50' maxLength='1000' placeholder='Resumo' required></textarea>
+        <textarea onMouseOver={alertMsg} onChange={handleChange} value={value.cargo} name='cargo' rows='2' cols='50' maxLength='40' placeholder='Cargo' required></textarea>
+        <textarea onChange={handleChange} value={value.descricao} name='descricao' rows='2' cols='50' maxLength='500' placeholder='Descricao' required></textarea>
+        <span>{error ? error : ''}</span>
+      </fieldset>
+      {/* <button onSubmit={sendButton}>Submit</button> */}
+      <button onSubmit={resetButton}>Reset</button>
     </div>
     )
   }
